54b525a8a37fd36ad8d2cc809da3b126625001d3
toyotadriver <polaemil@gmail.com>
2024-11-30 13:46:59 +0200

    "added supabase"

---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://nrxbmaxrznnmckmxadqn.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5yeGJtYXhyem5ubWNrbXhhZHFuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzI4OTA3MjAsImV4cCI6MjA0ODQ2NjcyMH0.Uk5wj3oibf2_4shTRER464IAzzJhlMKnNYqwKnX9y_0";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
