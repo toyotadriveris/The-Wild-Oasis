653e3f4a251472c24883a8e5745cb31553b9026d
toyotadriver <polaemil@gmail.com>
2024-12-03 17:21:27 +0200

    ""

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -1,12 +1,24 @@
+import { useSearchParams } from "react-router-dom";
 import { getToday } from "../utils/helpers";
 import supabase from "./supabase";
 
-export async function getBookings() {
-  const { data, error } = await supabase
+export async function getBookings({ filter, sortBy }) {
+  let query = supabase
     .from("bookings")
     .select(
       "id,created_at, startDate, endDate, numNights, numGuests, status,totalPrice, cabins(name), guests(fullName,email)"
     );
+
+  // Filter
+  if (filter) query = query.eq(filter.field, filter.value);
+
+  // Sort
+  if (sortBy)
+    query = query.order(sortBy.field, {
+      ascending: sortBy.direction === "asc",
+    });
+
+  const { data, error } = await query;
 
   if (error) {
     console.error(error);
