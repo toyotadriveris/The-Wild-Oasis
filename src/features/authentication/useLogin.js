190d9037f51ee47570e3078abcb065b7f519b86f
toyotadriver <polaemil@gmail.com>
2024-12-05 17:29:56 +0200

    ""

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -1,11 +1,11 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { login as loginApi } from "../../services/apiAuth";
 import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
 
 export function useLogin() {
   const navigate = useNavigate();
-
+  const queryClient = useQueryClient();
   const { mutate: login, isLoading } = useMutation({
     mutationFn: ({ email, password }) =>
       loginApi({
@@ -13,8 +13,8 @@ export function useLogin() {
         password,
       }),
     onSuccess: (user) => {
-      console.log(user);
-      navigate("/dashboard");
+      queryClient.setQueryData(["user"], user.user);
+      navigate("/dashboard", { replace: true });
     },
     onError: (err) => {
       console.log("error", err);
