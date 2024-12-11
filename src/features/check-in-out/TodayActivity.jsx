2952a459755f63173b4570d484e60115460612ae
toyotadriver <polaemil@gmail.com>
2024-12-11 06:36:34 +0200

    ""

--- src/features/check-in-out/TodayActivity.jsx
+++ src/features/check-in-out/TodayActivity.jsx
@@ -3,7 +3,6 @@ import styled from "styled-components";
 import Heading from "../../ui/Heading";
 import Row from "../../ui/Row";
 import { useTodayActivity } from "./useTodayActivity";
-import SpinnerMini from "../../ui/SpinnerMini";
 import Spinner from "../../ui/Spinner";
 import TodayItem from "./TodayItem";
 const StyledToday = styled.div`
--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 
